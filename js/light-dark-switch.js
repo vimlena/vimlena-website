e7fc55dc4c6c75de6fd47c94ba084ffbb2c9d5a8
Vim Lena <veiakurman@gmail.com>
2025-08-19 11:27:35 -0500

    Fix theme selection bug

--- js/light-dark-switch.js
+++ js/light-dark-switch.js
@@ -9,23 +9,17 @@ let prefersHC = window.matchMedia("(prefers-contrast: more)");
 window.onload = function() {
     if (window.matchMedia && prefersLight.matches && prefersHC.matches) {
         document.body.classList.add("lightmodehc");
-    } else if (window.matchMedia && prefersLight.matches && !prefersHC.matches) {
-        document.body.classList.add("lightmodehc");
-    } else if (window.matchMedia && prefersDark.matches && prefersHC.matches) {
+    }
+    if (window.matchMedia && prefersDark.matches && prefersHC.matches) {
         document.body.classList.add("darkmodehc");
-    } else {
-        document.body.classList.add("darkmode");
     }
 };
 /* if user wants a light, high contrast theme
  *     add "lightmodehc" to body
- * else if user wants a light, non-high contrast theme
- *     add "lightmode" to body
- * else if user wants a dark, high contrast theme
+ * if user wants a dark, high contrast theme
  *     add "darkmodehc" to body
- * else
- *     add "darkmode" to body
  * end
+ * 
  * these color/contrast themes are all defined in ../css/stylesheet.css from
  * lines 68-101
  */
